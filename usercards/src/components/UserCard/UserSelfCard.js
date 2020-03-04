342eb48783b1fb3a182ad91609f16bf39b04367a
Teghfo <example@example.com>
2020-03-04 11:41:24 +0330

    userSelfCard edit

--- usercards/src/components/UserCard/UserSelfCard.js
+++ usercards/src/components/UserCard/UserSelfCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./UserSelfCard.module.scss";
 
 const UserSelfCard = ({ users }) => {
+  const [textStatus, setTExtStatus] = useState("name");
+  const [textContent, setTextContent] = useState({
+    header: "Hi, My name is",
+    content: `${users.name.first} ${users.name.last}`
+  });
   return (
     <>
       <div className={styles.card}>
         <div>
-          <img src={users.picture.large} />
-          <p />
-          <p />
+          <img className={styles.image} src={users.picture.large} />
         </div>
-
+        <div className={styles.detail}>
+          <p className={styles.header}>{textContent.header}</p>
+          <p className={styles.content}>{textContent.content}</p>
+        </div>
         <div></div>
       </div>
     </>

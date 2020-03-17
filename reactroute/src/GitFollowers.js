e47ce686a9ea8e2e7bb6f1875bcc32788b022d97
Teghfo <example@example.com>
2020-03-17 22:42:38 +0330

    minor fix

--- reactroute/src/GitFollowers.js
+++ reactroute/src/GitFollowers.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useFetch from "./useFetch";
 
 export default function GitFollowers({ username }) {
-
-  const { data, loading, error } = useFetch(`https://api.github.com/users/${username}/followers`);
-  console.log(data, loading, error);
+  const { data, loading, error } = useFetch(
+    `https://api.github.com/users/${username}/followers`
+  );
+  useEffect(() => {
+    console.log(data, loading, error);
+  });
   return <div></div>;
 }
